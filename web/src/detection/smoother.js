309497ea729afdbcc37679310151958b28b8f413
Ken Kawamoto <ken@kawamoto.co.uk>
2021-03-17 15:46:29 +0900

    Implements smoothing (O(n^2) greedy) for multi-pose tracking

--- web/src/detection/smoother.js
+++ web/src/detection/smoother.js
@@ -14,11 +14,16 @@ class FeatureSmoother {
         this.points[this.tail] = position;
 
         this.tail = (this.tail + 1) % this.window;
+        return this.smoothed();
+    }
+    num() {
+        return this.points.length;
     }
     smoothed() {
         const head = this.points.length < this.window ? 0 : this.tail;
         const numPoints = Math.min(this.points.length, this.window);
 
+        // TODO: make this incremental.
         let sumX = 0;
         let sumY = 0;
         for (let i = 0; i < numPoints; i++) {
@@ -33,4 +38,30 @@ class FeatureSmoother {
         };
     }
 }
-export default FeatureSmoother;
+
+class PoseSmoother {
+    constructor(smoothingWindow) {
+        this.smoothingWindow = smoothingWindow;
+        this.smootherForFeature = {};
+    }
+    smoother(feature) {
+        let smoother = this.smootherForFeature[feature.part];
+        if (!smoother) {
+            this.smootherForFeature[feature.part] = smoother = new FeatureSmoother(this.smoothingWindow);
+        }
+        return smoother;
+    }
+    add(feature) {
+        const smoother = this.smoother(feature);
+        return smoother.add(feature.position);
+    }
+    smooth(pose) {
+        pose.keypoints.forEach(feature => {
+            const smoothed = this.add(feature);
+            if (smoothed) {
+                feature.position = smoothed;
+            }
+        });
+    }
+}
+export default PoseSmoother;
